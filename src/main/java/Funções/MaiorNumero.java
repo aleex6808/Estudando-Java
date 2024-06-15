b2c857049ec464ff9726692c2b067fddda3f82b3
aleex6808 <aleex6808@gmail.com>
2024-06-15 11:47:15 -0300

    Update and rename maiorNumero.java to MaiorNumero.java

--- src/main/java/Funções/MaiorNumero.java
+++ src/main/java/Funções/MaiorNumero.java
@@ -12,7 +12,7 @@ package Funções;
 
 import java.util.Scanner;
 
-class maiorNumero{
+class MaiorNumero{
   public static void main(String[] args) {
     int n1, n2, n3;
     Scanner entrada = new Scanner(System.in);
