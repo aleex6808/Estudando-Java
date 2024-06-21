88a0d93619fb12956075b8e7b4bbd360a4ea1db4
aleex6808 <aleex6808@gmail.com>
2024-06-21 13:06:29 +0000

    SGT

--- src/main/java/Projetinhos_:)/SGT.java
+++ src/main/java/Projetinhos_:)/SGT.java
@@ -1,4 +1,4 @@
- /*
+/*
 Projeto: Sistema de Gerenciamento de Tarefas
 Descrição
 Crie um sistema de gerenciamento de tarefas simples em Java que permita ao usuário adicionar, visualizar, atualizar e excluir tarefas. Este projeto irá ajudá-lo a aplicar conceitos de variáveis, tipos de dados, operadores, estruturas de controle, funções/métodos, listas/arrays e manipulação de strings.
@@ -27,7 +27,6 @@ Armazena as tarefas em uma lista (array list).
 
 import java.util.Scanner;
 
-
 class SGT {
   public static void main(String[] args) {
     final int TAM = 10;
@@ -35,8 +34,8 @@ class SGT {
     String[] tarefa = new String[TAM], descricao = new String[TAM];
     int i = 0, opcao = 0, contTarefas = 0, atualizar = 0;
     int opcao2 = 0, excluir = 0;
-    String novoTitulo, novaDescricao;
-    while (opcao != 6) {
+    String novoTitulo, novaDescricao, aux;
+    while (opcao != 7) {
       menu();
       opcao = entrada.nextInt();
       entrada.nextLine();
@@ -44,13 +43,13 @@ class SGT {
         case 1:
           System.out.print("\nDigite o título da tarefa: ");
           tarefa[i] = entrada.nextLine();
-          if(tarefa[i].equals("")){
+          if (tarefa[i].equals("")) {
             System.out.println("Erro!! Campo vazio..");
             break;
           }
           System.out.print("\nDigite a descrição da tarefa: ");
           descricao[i] = entrada.nextLine();
-          if(descricao[i].equals("")){
+          if (descricao[i].equals("")) {
             System.out.println("Erro!! Campo vazio..");
             break;
           }
@@ -59,15 +58,15 @@ class SGT {
           System.out.print("\nTarefa adicionada com sucesso!");
           break;
         case 2:
-            System.out.println("-----------------------------------------");
-            for(i = 0; i < contTarefas; i++){
-              if (tarefa[i].equals("0")) {
-                } else {
-              System.out.println("\nTarefa " + (i+1) + ": " + tarefa[i] + "");
-              System.out.println("Descrição " + (i+1) + ": " + descricao[i] + "\n");
+          System.out.println("-----------------------------------------");
+          for (i = 0; i < contTarefas; i++) {
+            if (tarefa[i].equals("0")) {
+            } else {
+              System.out.println("\nTarefa " + (i + 1) + ": " + tarefa[i] + "");
+              System.out.println("Descrição " + (i + 1) + ": " + descricao[i] + "\n");
             }
           }
-            System.out.println("-----------------------------------------");
+          System.out.println("-----------------------------------------");
           break;
         case 3:
           System.out.println("\n1 - Atualizar o título ");
@@ -75,83 +74,95 @@ class SGT {
 
           System.out.print("\nEscolha uma das opções acima: ");
           opcao2 = entrada.nextInt();
-          
+
           System.out.print("\nDigite o número da tarefa que deseja atualizar: ");
           atualizar = entrada.nextInt();
           entrada.nextLine();
 
-          switch(opcao2) {
-              
-            case 1: 
+          switch (opcao2) {
+
+            case 1:
               System.out.print("\nDigite o novo título da tarefa: ");
               novoTitulo = entrada.nextLine();
 
-              tarefa[atualizar-1] = novoTitulo;
+              tarefa[atualizar - 1] = novoTitulo;
               break;
-              
+
             case 2:
               System.out.print("\nDigite a nova descrição da tarefa: ");
               novaDescricao = entrada.nextLine();
 
-              descricao[atualizar-1] = novaDescricao;
+              descricao[atualizar - 1] = novaDescricao;
             default:
               System.out.println("Opcão inválida");
               break;
           }
           System.out.print("\nTarefa atualizada com sucesso!");
           break;
-          
+
         case 4:
           System.out.print("\nDigite o número da tarefa que deseja excluir: ");
           excluir = entrada.nextInt();
           entrada.nextLine();
 
-          tarefa[excluir-1] = "0";
+          tarefa[excluir - 1] = "0";
           System.out.print("\nTarefa excluída com sucesso!");
           break;
-          
+
         case 5:
           System.out.println("Digite o título da tarefa que deseja buscar: ");
           String buscar = entrada.nextLine();
           System.out.println("\n-----------------------------------------");
-          for(i = 0; i < contTarefas; i++) {
-            if(buscar.equalsIgnoreCase(tarefa[i])){
-              System.out.println("\nTarefa " + (i+1) + ": " + tarefa[i] + "");
-              System.out.println("Descrição " + (i+1) + ": " + descricao[i] + "\n");
+          for (i = 0; i < contTarefas; i++) {
+            if (buscar.equalsIgnoreCase(tarefa[i])) {
+              System.out.println("\nTarefa " + (i + 1) + ": " + tarefa[i] + "");
+              System.out.println("Descrição " + (i + 1) + ": " + descricao[i] + "\n");
             }
           }
           System.out.println("-----------------------------------------\n");
           break;
-          
+
         case 6:
-          for (i = 0; i < contTarefas; i++){
-            for (int j = 0; j < i; j++){
-              
+          for (i = 0; i < contTarefas; i++) {
+            for (int j = 0; j < (contTarefas - 1); j++) {
+              if (tarefa[j].compareTo(tarefa[j + 1]) > 0) {
+                aux = tarefa[j];
+                tarefa[j] = tarefa[j + 1];
+                tarefa[j + 1] = aux;
+              }
             }
           }
+
+          for (i = 0; i < contTarefas; i++){
+            System.out.println("\n-----------------------------------------");
+            System.out.println("\nTarefa " + (i + 1) + ": " + tarefa[i] + "");
+            System.out.println("Descrição " + (i + 1) + ": " + descricao[i] + "\n");
+            System.out.println("\n-----------------------------------------");
+          }
           break;
-          
+
         case 7:
           System.out.println("\n--- Fim do programa!! ---\n");
           break;
-          
+
         default:
           System.out.println("Opcão inválida");
           break;
       }
-      
+
     }
     entrada.close();
   }
 
   public static void menu() {
-    System.out.println("--- Sistema de Gerenciamento de Tarefas ---");
+    System.out.println("\n--- Sistema de Gerenciamento de Tarefas ---\n");
     System.out.println(" 1 - Adicionar Tarefa ");
     System.out.println(" 2 - Visualizar Tarefa(s) ");
     System.out.println(" 3 - Atualizar Tarefa ");
     System.out.println(" 4 - Excluir Tarefa ");
     System.out.println(" 5 - Buscar Tarefa ");
-    System.out.println(" 6 - Sair");
+    System.out.println(" 6 - Ordenar Alfabeticamente a lista ");
+    System.out.println(" 7 - Sair");
     System.out.print("\n Digite aqui: ");
   }
 }
