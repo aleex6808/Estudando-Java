5b63f8d6ce0f302b73ea600397267577d0bf0de7
aleex6808 <aleex6808@gmail.com>
2024-06-26 08:27:03 -0300

    Alteração de arquivos e criação de nova pasta

---
+++ src/main/java/POO/ClasseQueCalcula.java
@@ -0,0 +1,18 @@
+package POO;
+
+
+
+public class ClasseQueCalcula {
+		
+	
+	public int calcularArea(int base, int altura) {
+		return base * altura;
+	}
+	
+	//Versão do método para parâmetros de ponto flutuante
+	
+	public double calcularArea(double base, double altura) {
+		return base * altura;
+		
+	}
+}
